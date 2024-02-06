f09852fdde0bb3ab0b19f1076abdb35afe9e7af3
Ekuabaah <baahekua5@gmail.com>
2024-02-06 20:00:54 +0000

    sabi girl

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Where the code starts
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = '0'; m <= '9'; m++)
+		printf("%d", m);
+	printf("\n");
+	return (0);
+}
