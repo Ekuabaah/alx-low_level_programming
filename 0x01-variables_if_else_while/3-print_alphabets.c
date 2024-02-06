d50def808d1462371d2e7372b2175ec722ef1ec4
Ekuabaah <baahekua5@gmail.com>
2024-02-06 19:13:33 +0000

    a to z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Where the code starts
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphas;
+	for (alphas = 'a'; alphas <= 'z'; alphas++)
+		putchar(alphas);
+	for (alphas = 'A'; alphas <= 'Z'; alphas++)
+		putchar(alphas);
+	putchar('\n');
+	return (0);
+}
